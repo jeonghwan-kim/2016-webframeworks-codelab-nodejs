37e20db6e8399e8baffc0b0684b7d80f6611ef2b
jeonghwan-kim <ej88ej@gmail.com>
2016-09-09 17:16:00 +0900

    should

--- api/user/user.spec.js
+++ api/user/user.spec.js
@@ -1,5 +1,6 @@
 const assert = require('assert');
 const request = require('supertest');
+const should = require('should');
 const app = require('../../app');
 
 describe('GET /users', () => {
@@ -16,7 +17,75 @@ describe('GET /users', () => {
         .expect(200)
         .end((err, res) => {
           if (err) throw err;
-          assert.equal((res.body instanceof Array), true);
+          res.body.should.be.an.instanceof(Array).and.have.length(3);
+          res.body.map(user => {
+            user.should.have.properties('id', 'name');
+            user.id.should.be.a.Number();
+            user.name.should.be.a.String();
+          })
+          done();
+        });
+  });
+});
+
+describe('GET /users/:id', () => {
+  it('should return 200 status code', (done) => {
+    request(app)
+        .get('/users/1')
+        .expect(200)
+        .end(done);
+  });
+
+  it('should return 404 status code', (done) => {
+    request(app)
+        .get('/users/4')
+        .expect(404)
+        .end((err, res) => {
+          if (err) throw err;
+          res.body.should.have.property('error');
+          done();
+        });
+  });
+
+  it('should return object', (done) => {
+    request(app)
+        .get('/users/1')
+        .expect(200)
+        .end((err, res) => {
+          if (err) throw err;
+          res.body.should.be.an.instanceof(Object)
+          res.body.should.have.properties('id', 'name');
+          res.body.id.should.be.a.Number();
+          res.body.name.should.be.a.String();
+          done();
+        });
+  });
+});
+
+describe('POST /users', () => {
+  it('should return 201 status code', (done) => {
+    request(app)
+        .post('/users')
+        .send({
+          name: 'foo'
+        })
+        .expect(201)
+        .end(done);
+  });
+
+  it('should return new user object', (done) => {
+    request(app)
+        .post('/users')
+        .send({
+          name: 'foo'
+        })
+        .expect(201)
+        .end((err, res) => {
+          if (err) throw err;
+          res.body.should.be.an.instanceof(Object)
+          res.body.should.have.properties('id', 'name');
+          res.body.id.should.be.a.Number();
+          res.body.name.should.be.a.String();
           done();
         });
   });
