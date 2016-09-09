1ceaf8a507bd7047c2c402c6a495a9fb596e0b4c
jeonghwan-kim <ej88ej@gmail.com>
2016-09-09 15:24:55 +0900

    Express

--- app.js
+++ app.js
@@ -1,14 +1,10 @@
-const http = require('http');
+const express = require('express');
+const app = express();
 
-const hostname = '127.0.0.1';
-const port = 3000;
-
-const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello World\n');
+app.get('/', (req, res) => {
+  res.send('Hello World!\n');
 });
 
-server.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
+app.listen(3000, () => {
+  console.log('Example app listening on port 3000!');
 });
