d25384346843a067f136b702afd22ba92b10e5ac
jeonghwan-kim <ej88ej@gmail.com>
2016-09-26 14:39:23 +0900

    sequalize in controller

--- api/user/user.controller.js
+++ api/user/user.controller.js
@@ -1,44 +1,34 @@
-// Database Mockup
-let users = [
-  {
-    id: 1,
-    name: 'chris',
-  },
-  {
-    id: 2,
-    name: 'tim',
-  },
-  {
-    id: 3,
-    name: 'daniel'
-  }
-]
+const models = require('../../models');
 
 exports.show = (req, res) => {
   const id = parseInt(req.params.id, 10);
   if (!id) return res.status(400).json({error: 'Incorrect id'});
 
-  let user = users.filter(user => user.id === id)[0]
-  if (!user) return res.status(404).json({error: 'Unknown user'});
-
-  return res.json(user);
+  models.User.findOne({
+    where: {
+      id: id
+    }
+  }).then(user => {
+    if (!user) {
+      return res.status(404).json({error: 'No User'});
+    }
+    return res.json(user);
+  });
 };
 
 exports.index = (req, res) => {
-  res.json(users)
+  models.User.findAll().then(users => res.json(users));
 };
 
 exports.destroy = (req, res) => {
   const id = parseInt(req.params.id, 10);
   if (!id) return res.status(400).json({error: 'Incorrect id'});
 
-  const userIdx = users.findIndex(user => user.id === id);
-  if (userIdx === -1) {
-    return res.status(404).json({error: 'Unknown user'});
-  }
-
-  users.splice(userIdx, 1);
-  res.status(204).send();
+  models.User.destroy({
+    where: {
+      id: id
+    }
+  }).then(() => res.status(204).send());
 };
 
 exports.create = (req, res) => {
@@ -47,18 +37,9 @@ exports.create = (req, res) => {
     return res.status(400).json({error: 'Incorrenct name'});
   }
 
-  const id = users.reduce((maxId, user) => {
-    return user.id > maxId ? user.id : maxId
-  }, 0) + 1;
-
-  const newUser = {
-    id: id,
+  models.User.create({
     name: name
-  };
-
-  users.push(newUser);
-
-  return res.status(201).json(newUser);
+  }).then((user) => res.status(201).json(user))
 };
 
 exports.update = (req, res) => {
