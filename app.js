49cfc76976ecb9f112ed42858695de8465342cc7
jeonghwan-kim <ej88ej@gmail.com>
2016-09-26 13:56:18 +0900

    DELETE /users/:id

--- app.js
+++ app.js
@@ -1,9 +1,5 @@
 const express = require('express');
-const bodyParser = require('body-parser');
 const app = express();
-
-app.use(bodyParser.json()); // for parsing application/json
-app.use(bodyParser.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
 
 // Database Mockup
 let users = [
@@ -47,44 +43,6 @@ app.delete('/users/:id', (req, res) => {
   users.splice(userIdx, 1);
   res.status(204).send();
 });
-
-app.post('/users', (req, res) => {
-  const name = req.body.name || '';
-  if (!name.length) {
-    return res.status(400).json({error: 'Incorrenct name'});
-  }
-
-  const id = users.reduce((maxId, user) => {
-    return user.id > maxId ? user.id : maxId
-  }, 0) + 1;
-
-  const newUser = {
-    id: id,
-    name: name
-  };
-
-  users.push(newUser);
-
-  return res.status(201).json(newUser);
-});
-
-app.put('/users/:id', (req, res) => {
-  const id = parseInt(req.params.id, 10);
-  if (!id) return res.status(400).json({error: 'Incorrect id'});
-
-  const name = req.body.name || '';
-  if (!name.length) {
-    return res.status(400).json({error: 'Incorrenct name'});
-  }
-
-  let user = users.filter(user => user.id === id)[0];
-  if (!user) {
-    return res.status(404).json({error: 'Unknown user'});
-  }
-
-  user.name = name;
-  return res.json(user);
-})
 
 app.listen(3000, () => {
   console.log('Example app listening on port 3000!');
