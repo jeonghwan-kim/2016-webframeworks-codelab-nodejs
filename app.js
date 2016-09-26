fb82d1a26c29e232d530c2c9ac64b187b914387a
jeonghwan-kim <ej88ej@gmail.com>
2016-09-26 14:35:21 +0900

    sequelize model

--- app.js
+++ app.js
@@ -9,6 +9,11 @@ app.use('/users', require('./api/user'));
 
 app.listen(3000, () => {
   console.log('Example app listening on port 3000!');
+
+  require('./models').sequelize.sync({force: true})
+      .then(() => {
+        console.log('Databases sync');
+      });
 });
 
 module.exports = app;
